f9ce5819345dcffbb3daabba2e22b13a6c6347c0
luna <paulkingdev7@gmail.com>
2024-07-14 23:08:47 +0300

    styling

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,7 +1,34 @@
 import React from "react";
 
 function Projects() {
-  return <div>Projects</div>;
+  return (
+    <>
+      <div>
+        <h1 className="text-base">Projects</h1>
+        <p className="flex py-2 space-x-2 text-xl ">
+          <div className="flex flex-col p-2 bg-slate-50/5 w-[250px] rounded-xl">
+            <span>Fleet</span>
+            <span className="text-base">A telematics system</span>
+          </div>
+          <div className="flex flex-col p-2 bg-slate-50/5 w-[250px] rounded-xl">
+            <span>FutureHive Smart Solution</span>
+            <span className="text-base">
+              This is an e-commerce website to handle sales
+            </span>
+          </div>
+          <div className="flex flex-col p-2 bg-slate-50/5 w-[250px] rounded-xl">
+            <span>Name Project</span>
+            <span className="text-base">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus
+              recusandae non nihil dicta et ipsum voluptates amet quisquam
+              molestiae blanditiis? Dolor dolore dicta totam quisquam maxime ut
+              labore, a tenetur!
+            </span>
+          </div>
+        </p>
+      </div>
+    </>
+  );
 }
 
 export default Projects;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,7 +10,7 @@ function Home() {
         <NameHolder />
         <div className="flex items-center justify-center mt-2 text-xs ">
           <FingerPrintIcon className="w-5 h-5 mr-1 " />
-          Built and designed by Kinyatti paul
+          Built and designed by Kinyatti Paul
         </div>
       </div>
     </>
